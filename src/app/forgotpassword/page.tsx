03c01252864a766559d7d7995d3dd6cc33b504a7
Maya Saltzman <mayasaltzman3@gmail.com>
2024-03-11 12:17:42 -0400

    added seperate file and folder for password reset page

---
+++ src/app/forgotpassword/page.tsx
@@ -0,0 +1,17 @@
+import { JSX } from "react";
+
+export default function forgotpassword(): JSX.Element {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      {/* form for info for password reset */}
+      <div>
+        <form action="/passwordreset" method="POST">
+          <input type="email" name="email" value="" placeholder="Enter your email address..." />
+          <input type="submit" value="Reset Password" />
+        </form>
+      </div>
+    </main>
+
+  );
+}
