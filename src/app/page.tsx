689e2c10cbc77a18fae33ef16237026cf34ac791
Maya Saltzman <mayasaltzman3@gmail.com>
2024-03-11 12:10:53 -0400

    added temp password reset html to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import { JSX } from "react";
 
 export default function Home(): JSX.Element {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div>
+        <form action="/passwordreset" method="POST">
+          <input type="email" name="email" value="" placeholder="Enter your email address..." />
+          <input type="submit" value="Reset Password" />
+        </form>
+      </div>
+    </main>
+
   );
 }
--- src/lib/jwt/index.js
+++ src/lib/jwt/index.js
@@ -6,16 +6,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// create webserver that allows user to reset password (temporary will see how this works with actual side)
-app.listen(3000, function () {
-    console.log('Node started on port 3000!')
-    console.log("test")
-});
 
-//form that asks user for their email address to begin password reset
-app.get('/forgotpassword', function (req, res) {
-    res.send('<form action="/passwordreset" method="POST">' +
-        '<input type="email" name="email" value="" placeholder="Enter your email address..." />' +
-        '<input type="submit" value="Reset Password" />' +
-        '</form>');
-});
+
+
+
