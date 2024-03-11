59f2efd37dea7fd63a93e5a5721f8bae733163d9
Maya Saltzman <mayasaltzman3@gmail.com>
2024-03-11 12:24:39 -0400

    wrote outline of what I am going to do will get help implementing at work session

--- src/lib/jwt/index.js
+++ src/lib/jwt/index.js
@@ -7,6 +7,25 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
+app.post('/passwordreset', function (req, res) {
+    //get users email from for data
+    //find user in database
+    //make one time token
+    //hash current password from database 
+    //make secret with the users password and the date created
+    //encode the key
+    //end email with link to rest 
+})
+
+//get data from email
+app.get('/resetpassword/:id/:token', function(req, res) {
+
+})
+
+//handle forms post with new password
+app.post('/resetpassword', function(req, res){
+
+})
 
 
 
